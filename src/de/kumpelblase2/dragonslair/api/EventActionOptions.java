fa892032cf5dbfd57426f2391b4d5750764fa0e5
Tim H <admin@infinityblade.de>
2012-07-02 21:15:23 +0200

    Small changes

--- src/de/kumpelblase2/dragonslair/api/EventActionOptions.java
+++ src/de/kumpelblase2/dragonslair/api/EventActionOptions.java
@@ -2,36 +2,37 @@ package de.kumpelblase2.dragonslair.api;
 
 import org.bukkit.ChatColor;
 
-
 public enum EventActionOptions
 {
-	NPC_SPAWN(new String[] { "npc_id" }, new String[] { "cooldown", "delay" }),
-	BLOCK_CHANGE(new String[] { "world", "block_id", "x", "y", "z" }, new String[] { "x2", "y2", "z2", "cooldown", "delay" }),
-	MOB_SPAWN(new String[] { "world", "mob_id", "x", "y", "z" }, new String[] { "amount", "cooldown", "delay" }),
-	OBJECTIVE_COMPLETE(new String[] { "objective_id" }, new String[] { "cooldown", "delay" }),
-	NPC_DESPAWN(new String[] { "npc_id" }, new String[] { "cooldown", "delay" }),
-	ITEM_REMOVE(new String[] { "item_id", "amount" }, new String[] { "scope", "on_failure", "on_success", "cooldown", "delay" }),
-	PLAYER_WARP(new String[] { "world", "x", "y", "z" }, new String[] { "scope", "cooldown", "delay" }),
-	DUNGEON_START(new String[] { "dungeon_id" }, new String[] { "cooldown", "delay" }),
-	DUNGEON_END(new String[] { "dungeon_id" }, new String[] { "warp_on_end", "give_items", "cooldown", "delay" }),
-	DUNGEON_REGISTER(new String[] { "dungeon_id" }, new String[] { "cooldown", "delay" }),
-	ITEM_ADD(new String[] { "item_id", "amount" }, new String[] { "scope", "damage", "cooldown", "delay" }),
-	CHAPTER_COMPLETE(new String[] { "chapter_id" }, new String[] { "cooldown", "delay" }),
-	NPC_ATTACK(new String[] { "npc_id" }, new String[] { "target", "cooldown", "delay" }),
-	NPC_STOP_ATTACK(new String[] { "npc_id" }, new String[] { "cooldown", "delay" }),
-	NPC_WALK(new String[] { "npc_id", "x", "y", "z" }, new String[] { "cooldown", "delay" }),
-	NPC_DIALOG(new String[] { "npc_id", "dialog_id" }, new String[] { "send_to", "cooldown", "delay" }),
-	ITEM_SPAWN(new String[] { "world", "x", "y", "z", "item_id" }, new String[] { "amount", "cooldown", "delay" }),
-	BROADCAST_MESSAGE(new String[] { "message" }, new String[] { "permission", "dungeon_id", "cooldown", "delay" }),
-	SAY(new String[] { "message" }, new String[] { "cooldown", "delay" }),
-	ADD_POTION_EFFECT(new String[] { "potiontype", "amplifier", "duration" }, new String[] { "scope", "cooldown", "delay" }),
-	REMOVE_POTION_EFFECT(new String[] { "potiontype" }, new String[] { "duration", "scope", "cooldown", "delay" }),
-	CHANGE_LEVEL(new String[] { "amount", "change_type" }, new String[] { "scope", "cooldown", "delay" }),
-	CHANGE_HEALTH(new String[] { "amount", "change_type" }, new String[] { "scope", "cooldown", "delay" }),
-	CHANGE_HUNGER(new String[] { "amout", "change_type" }, new String[] { "scope", "cooldown", "delay" });
+	NPC_SPAWN(new String[] { "npc_id" }, new String[0]),
+	BLOCK_CHANGE(new String[] { "world", "block_id", "x", "y", "z" }, new String[] { "x2", "y2", "z2" }),
+	MOB_SPAWN(new String[] { "world", "mob_id", "x", "y", "z" }, new String[] { "amount" }),
+	OBJECTIVE_COMPLETE(new String[] { "objective_id" }, new String[0]),
+	NPC_DESPAWN(new String[] { "npc_id" }, new String[0]),
+	ITEM_REMOVE(new String[] { "item_id", "amount" }, new String[] { "scope", "on_failure", "on_success" }),
+	PLAYER_WARP(new String[] { "world", "x", "y", "z" }, new String[] { "scope" }),
+	DUNGEON_START(new String[] { "dungeon_id" }, new String[0]),
+	DUNGEON_END(new String[] { "dungeon_id" }, new String[] { "warp_on_end", "give_items" }),
+	DUNGEON_REGISTER(new String[] { "dungeon_id" }, new String[0]),
+	ITEM_ADD(new String[] { "item_id", "amount" }, new String[] { "scope", "damage" }),
+	CHAPTER_COMPLETE(new String[] { "chapter_id" }, new String[0]),
+	NPC_ATTACK(new String[] { "npc_id" }, new String[] { "target" }),
+	NPC_STOP_ATTACK(new String[] { "npc_id" }, new String[0]),
+	NPC_WALK(new String[] { "npc_id", "x", "y", "z" }, new String[0]),
+	NPC_DIALOG(new String[] { "npc_id", "dialog_id" }, new String[] { "send_to" }),
+	ITEM_SPAWN(new String[] { "world", "x", "y", "z", "item_id" }, new String[] { "amount" }),
+	BROADCAST_MESSAGE(new String[] { "message" }, new String[] { "permission", "dungeon_id" }),
+	SAY(new String[] { "message" }, new String[0]),
+	ADD_POTION_EFFECT(new String[] { "potiontype", "amplifier", "duration" }, new String[] { "scope" }),
+	REMOVE_POTION_EFFECT(new String[] { "potiontype" }, new String[] { "duration", "scope" }),
+	CHANGE_LEVEL(new String[] { "amount", "change_type" }, new String[] { "scope" }),
+	CHANGE_HEALTH(new String[] { "amount", "change_type" }, new String[] { "scope" }),
+	CHANGE_HUNGER(new String[] { "amout", "change_type" }, new String[] { "scope" }),
+	EXECUTE_COMMAND(new String[] { "command" }, new String[] { "execute_as" });
 	
 	private String[] requiredOptions;
 	private String[] optionalOptions;
+	private final String[] generalOptions = new String[] { "delay", "cooldown" };
 	
 	private EventActionOptions(String[] required, String[] optional)
 	{
@@ -51,7 +52,10 @@ public enum EventActionOptions
 	
 	public String[] getOptionalTypes()
 	{
-		return this.optionalOptions;
+		String[] temp = new String[this.generalOptions.length + this.optionalOptions.length];
+		System.arraycopy(this.optionalOptions, 0, temp, 0, this.optionalOptions.length);
+		System.arraycopy(this.generalOptions, 0, temp, this.optionalOptions.length, this.generalOptions.length);
+		return temp;
 	}
 	
 	public String getOptionString()
@@ -67,15 +71,27 @@ public enum EventActionOptions
 		
 		for(int i = 0; i < optionalOptions.length; i++)
 		{
-			sb.append(ChatColor.YELLOW + this.optionalOptions[i] + ChatColor.WHITE);
+			sb.append(ChatColor.YELLOW + this.optionalOptions[i] + ChatColor.WHITE + ", ");
+		}
+		
+		for(int i = 0; i < this.generalOptions.length; i++)
+		{
+			sb.append(ChatColor.YELLOW + this.generalOptions[i] + ChatColor.WHITE);
 			if(i != this.optionalOptions.length - 1)
 				sb.append(", ");
 		}
+		
 		return sb.toString();
 	}
 	
 	public boolean hasOption(String option)
 	{
+		for(String general : this.generalOptions)
+		{
+			if(general.equals(option))
+				return true;
+		}
+		
 		for(String required : this.requiredOptions)
 		{
 			if(required.equals(option))
