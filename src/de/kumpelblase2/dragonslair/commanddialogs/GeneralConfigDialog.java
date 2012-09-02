f2d5cf62b2e2feafebb83c172f69302a76cd8948
Tim H <admin@infinityblade.de>
2012-09-02 10:50:44 +0200

    Added small info in the config

--- src/de/kumpelblase2/dragonslair/commanddialogs/GeneralConfigDialog.java
+++ src/de/kumpelblase2/dragonslair/commanddialogs/GeneralConfigDialog.java
@@ -2,6 +2,7 @@ package de.kumpelblase2.dragonslair.commanddialogs;
 
 import org.bukkit.ChatColor;
 import org.bukkit.conversations.*;
+import de.kumpelblase2.dragonslair.DragonsLairMain;
 import de.kumpelblase2.dragonslair.commanddialogs.chapters.ChapterManageDialog;
 import de.kumpelblase2.dragonslair.commanddialogs.dialogs.DialogManageDialog;
 import de.kumpelblase2.dragonslair.commanddialogs.dungeon.DungeonManageDialog;
@@ -15,7 +16,7 @@ public class GeneralConfigDialog extends FixedSetPrompt
 {
 	public GeneralConfigDialog()
 	{
-		super("dungeons", "npcs", "events", "triggers", "chapters", "objectives", "dialogs", "scheduled events", "help", "exit");
+		super("dungeons", "npcs", "events", "triggers", "chapters", "objectives", "dialogs", "scheduled events", "help", "info", "exit");
 	}
 
 	@Override
@@ -24,7 +25,7 @@ public class GeneralConfigDialog extends FixedSetPrompt
 		arg0.getForWhom().sendRawMessage(ChatColor.GREEN + "Welcome to the configuration of Dragons Lair! You can quit this at any point by saying '/exit' in the chat.");
 		arg0.getForWhom().sendRawMessage(ChatColor.YELLOW + "Note: While you're in this configuration, you can't chat!");
 		arg0.getForWhom().sendRawMessage(ChatColor.GREEN + "What do you want to manage?");
-		return ChatColor.AQUA + "dungeons, npcs, events, triggers, chapters, objectives, dialogs, scheduled events, help, exit";
+		return ChatColor.AQUA + "dungeons, npcs, events, triggers, chapters, objectives, dialogs, scheduled events, help, info, exit";
 	}
 
 	@Override
@@ -48,6 +49,15 @@ public class GeneralConfigDialog extends FixedSetPrompt
 			return new DialogManageDialog();
 		else if(arg1.equals("scheduled events"))
 			return new ScheduledEventsManageDialog();
+		else if(arg1.equals("info"))
+		{
+			arg0.getForWhom().sendRawMessage(ChatColor.GOLD + "----------------------------------------------");
+			arg0.getForWhom().sendRawMessage(ChatColor.GOLD + "  Running Dragons Lair v" + DragonsLairMain.getInstance().getDescription().getVersion());
+			arg0.getForWhom().sendRawMessage(ChatColor.GOLD + "  Made by kumpelblase2");
+			arg0.getForWhom().sendRawMessage(ChatColor.GOLD + "  Have fun ;)");
+			arg0.getForWhom().sendRawMessage(ChatColor.GOLD + "----------------------------------------------");
+			return this;
+		}
 		else
 			return END_OF_CONVERSATION;
 	}
